28bc86983c92dc3b01f65fff046ea8b058f18b1f
KMicha <michael.kahle@yahoo.de>
2025-12-17 13:30:49 +0100

    Update newsapi.py

--- newsapi.py
+++ newsapi.py
@@ -495,7 +495,7 @@ def getLatestFileAge():
 
 ts = int(time.time())
 currentMonths = []
-for m in [0,20,40,60]:
+for m in range(0,100,20):
   ##month = datetime.utcfromtimestamp(ts-60*60*24*m).strftime('%Y_%m')  
   month = datetime.datetime.fromtimestamp(ts-60*60*24*m).strftime('%Y_%m')
   if month not in currentMonths:
